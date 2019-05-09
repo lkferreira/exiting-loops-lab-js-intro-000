ebd3b00b033da64bad38e6b51e891d45fb52d3f8
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-09 02:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
-    if (array[i] == findFN) {
+    if (array[i] == findFn) {
       return array[i]
     }
   }
