6f64126e739ca098f97fc7bbc6298f9d444a50db
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-09 02:36:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function breakOut(array, changeValue, stopValue) {
-  for (var i = 0; i < array.length) {
+  for (var i = 0; i < array.length; i++) {
     if(array[i] == stopValue) {
       break
     } else {
